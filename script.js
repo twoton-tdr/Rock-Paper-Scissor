cb9d7321a965f4309290652e2b7c74b4e3677b95
twoton tdr <79171113+twoton-tdr@users.noreply.github.com>
2024-06-23 18:09:43 +0530

    changed from console.log to alert and prompt

--- script.js
+++ script.js
@@ -2,21 +2,29 @@ let computerGuess;
 let humanGuess;
 let humanScore;
 let computerScore;
+let start;
+start = prompt("Click y to start the game");
+if(start =='y' || start == 'Y'){
+    rounds();
+}
+else{
+    alert('game not started');
+}
 function getComputerChoice(){
     computerGuess = Math.floor(Math.random()*9)+1;
-    // console.log(computerGuess);
+    
     if(computerGuess<4)
         {
-            // console.log("ROCK");
+           
             computerGuess= "ROCK";
         }
     else if(computerGuess>3 && computerGuess<7)
         {
-            // console.log("PAPER")
+            
             computerGuess="PAPER";
         }
     else {
-        // console.log("SCISSOR");
+        
         computerGuess="SCISSOR";
         
     }
@@ -26,45 +34,45 @@ function getHumanChoice(){
     humanGuess= prompt("Rock , Paper , Scissor");
     humanGuess= humanGuess.toUpperCase();
     
-    // console.log(humanGuess);
+   
 };
 function playround(humanChoice,computerChoice){
     console.log(humanChoice);
     console.log(computerChoice);
     if(humanChoice===computerChoice){
-        console.log('DRAW!!!!')
-        console.log(humanScore+' | '+computerScore)
+        alert('DRAW!!!!')
+        alert(humanScore+' | '+computerScore)
     }
     else{
         if(humanChoice==='ROCK' && computerChoice==='PAPER'){
-            console.log('computer beats human!!!!!');
+            alert('computer beats human!!!!!');
             computerScore = ++computerScore;
-            console.log(humanScore+' | '+computerScore)
+            alert(humanScore+' | '+computerScore)
         }
         else if(humanChoice==='ROCK' && computerChoice==='SCISSOR'){
-            console.log('human beats the computer!!!!');
+            alert('human beats the computer!!!!');
             humanScore = ++humanScore;
-            console.log(humanScore+' | '+computerScore)
+            alert(humanScore+' | '+computerScore)
         }
         else if(humanChoice==='PAPER' && computerChoice==='ROCK'){
-            console.log('human beats the computer!!!!');
+            alert('human beats the computer!!!!');
             humanScore = ++humanScore;
-            console.log(humanScore+' | '+computerScore)
+            alert(humanScore+' | '+computerScore)
         }
         else if(humanChoice==='PAPER' && computerChoice==='SCISSOR'){
-            console.log('computer beats the human!!!!');
+            alert('computer beats the human!!!!');
             computerScore = ++computerScore;
-            console.log(humanScore+' | '+computerScore)
+            alert(humanScore+' | '+computerScore)
         }
         else if(humanChoice==='SCISSOR' && computerChoice==='ROCK'){
-            console.log('computer beats the human!!!!');
+            alert('computer beats the human!!!!');
             computerScore = ++computerScore;
-            console.log(humanScore+' | '+computerScore);
+            alert(humanScore+' | '+computerScore);
         }
         else if(humanChoice==='SCISSOR' && computerChoice==='PAPER'){
-            console.log('human beats the computer!!!!');
+            alert('human beats the computer!!!!');
             humanScore = ++humanScore;
-            console.log(humanScore+' | '+computerScore)
+            alert(humanScore+' | '+computerScore)
         }
         else{
             alert("invalid input")
@@ -80,4 +88,10 @@ function rounds(){
     getHumanChoice();
     playround(humanGuess,computerGuess);
     };
+    if(humanScore<computerScore){
+        alert('computer wins!!!')
+    }
+    else{
+        alert('human wins!!!!!!')
+    }
 }
